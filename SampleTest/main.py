4ffc6d1adf6c20da5ce31126a617b23418cb8816
Derek Cover <dcover@mines.edu>
2021-11-02 15:21:41 -0600

    added ability to set variables from environment variables

--- SampleTest/main.py
+++ SampleTest/main.py
@@ -1,24 +1,34 @@
-import getopt
-import getpass
-import sys
+import getopt, getpass, sys, os
 
 # This is a sample Python script.
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
+def list_help():
+    print('test.py -i <inputfile> -o <outputfile> -e <extracmd>')
+    print('Options can also be passed as environment variables using the variable names in the <> above')
+
+
 def arg_check(argv):
-    if len(argv) == 0:
-        print('It looks like no argument/options were passed')
     try:
         opts, args = getopt.getopt(argv, "hi:o:e:",["ifile=","ofile=","excmd="])
+        # global variable scope - https://www.w3schools.com/python/python_variables_global.asp
+        global inputfile, outputfile, extracmd
     except getopt.GetoptError:
         print('Format the arguments as follows:')
-        print('test.py -i <inputfile> -o <outputfile> -e <extracommand>')
+        list_help()
         sys.exit(1)
     for opt, arg in opts:
-        print('Option: ' + opt)
-        print('Argument: ' + arg)
+        if opt in ('-h', '--help'):
+            list_help()
+            exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+        elif opt in ("-e", "--extracmd"):
+            extracmd = arg
 
 
 def print_hi(name):
@@ -32,5 +42,15 @@ if __name__ == '__main__':
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
+# Environment variables
+# https://www.nylas.com/blog/making-use-of-environment-variables-in-python/
+inputfile = os.getenv('inputfile')
+outputfile = os.getenv('outputfile')
+extracmd = os.getenv('extracmd')
 arg_check(sys.argv[1:])
 
+# Check if variable is None aka 'null'
+# https://pytutorial.com/check-if-variable-is-not-null-in-python
+print("Input file is " + str(inputfile).replace('None',''))
+print("Output file is " + str(outputfile).replace('None',''))
+print("Extra command is " + str(extracmd).replace('None',''))
